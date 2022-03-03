5062eda2c355b19ef3f65618d5929a0f14a9351f
Jesus Manuel Olivas (jmolivas) <jesus.olivas@gmail.com>
2022-03-02 23:13:29 -0800

    fix: h1 open/close tag

--- src/components/paragraph/paragraph-text.js
+++ src/components/paragraph/paragraph-text.js
@@ -8,10 +8,10 @@ export default function ParagraphText({ title, text }) {
                 <span className="mt-2 block text-4xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                     {title}
                 </span>
+            </h1>
                 <div className="mt-6 text-2xl prose max-w-6xl text-gray-500 mx-auto">
                     <div dangerouslySetInnerHTML={{ __html: text }} />
                 </div>
-            </h1>
         </div>
     )
 }
